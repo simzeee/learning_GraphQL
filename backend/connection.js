737aceb630d627d38373a78dd92bff21833930ca
John Sims <jwsims816@gmail.com>
2022-09-14 20:02:28 -0400

    env and connected to mongoDB

---
+++ backend/connection.js
@@ -0,0 +1,31 @@
+const { MongoClient } = require("mongodb");
+require('dotenv').config();
+
+async function main() {
+  /**
+   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+   */
+  const uri =
+    `mongodb+srv://jwsims:${process.env.PW}@simslearnmongo.vjgi8sm.mongodb.net/?retryWrites=true&w=majority`;
+
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    await listDatabases(client);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+
+async function listDatabases(client){
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+main().catch(console.error);
